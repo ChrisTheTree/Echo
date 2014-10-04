1b10a996ab6d1c63aae90b28446052b7fbaebeaa
guoruiwu <guorui_wu@hotmail.com>
2014-10-04 09:03:28 -0700

    Working with libraries

---
+++ IOS/Echo/Echo/Echo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Echo-Bridging-Header.h
+//  Echo
+//
+//  Created by Guorui Wu on 10-04-14.
+//  Copyright (c) 2014 TeamFC. All rights reserved.
+//
+
+#ifndef Echo_Echo_Bridging_Header_h
+#define Echo_Echo_Bridging_Header_h
+
+#import SocketIO.h
+
+#endif
