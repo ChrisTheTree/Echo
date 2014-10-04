d8af1640adbbc79dc046c0055cc48ece781e6eff
guoruiwu <guorui_wu@hotmail.com>
2014-10-04 07:29:51 -0700

    Start iOS Project

---
+++ IOS/Echo/Echo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Echo
+//
+//  Created by Guorui Wu on 10-04-14.
+//  Copyright (c) 2014 TeamFC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
