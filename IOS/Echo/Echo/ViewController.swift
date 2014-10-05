d4ec864b161961f82af4ab873e5480f674233ff1
guoruiwu <guorui_wu@hotmail.com>
2014-10-05 10:03:09 -0700

    Add new synching logic

--- IOS/Echo/Echo/ViewController.swift
+++ IOS/Echo/Echo/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreMedia
 import AVFoundation
+import Foundation
 
 class ViewController: UIViewController, SocketIODelegate {
     
@@ -21,16 +22,18 @@ class ViewController: UIViewController, SocketIODelegate {
     let host = "10.42.0.1"
     let port = 3030
     var songPath = "https://s3.amazonaws.com/alstroe/850920812.mp3"
-    var songPath2 = "https://s3.amazonaws.com/alstroe/850920812.mp3"
     var time = mach_timebase_info(numer: 0, denom: 0)
     var player = AVPlayer()
-    var player2 = AVPlayer()
     var playerItem = AVPlayerItem()
-    var playerItem2 = AVPlayerItem()
     var seekToTime = CMTimeMake(0, 1)
-    var seekToTime2 = CMTimeMake(0, 1)
     var offset: Double = 0
     var delay: Double = 0
+    let offsetTolerance: CLong = 10000
+    let numberOfOffsets: CLong = 10
+    let syncDelay: UInt32 = 40
+    var offsetCounter = 0
+    var offsets = [CLong](count: 10, repeatedValue: 0.0)
+    var synchronized: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,15 +47,7 @@ class ViewController: UIViewController, SocketIODelegate {
         self.playerItem = AVPlayerItem(URL: NSURL(string: self.songPath))
         self.player = AVPlayer(playerItem: playerItem)
         weak var weakSelf = self
-        waitThenRunOnMain(5.0) {
-            if let strongSelf = weakSelf {
-                strongSelf.player.prerollAtRate(1.0, completionHandler: nil)
-            }
-        }
-        
-        self.playerItem2 = AVPlayerItem(URL: NSURL(string: self.songPath2))
-        self.player2 = AVPlayer(playerItem: playerItem2)
-        waitThenRunOnMain(5.0) {
+        waitThenRunOnMain(10.0) {
             if let strongSelf = weakSelf {
                 strongSelf.player.prerollAtRate(1.0, completionHandler: nil)
             }
@@ -87,18 +82,7 @@ class ViewController: UIViewController, SocketIODelegate {
         } else if name == "offset" {
             let dict = packet.args[0] as NSDictionary
             self.offset = dict.objectForKey("offset") as Double
-            var t = mach_timebase_info(numer: 0, denom: 0)
-            mach_timebase_info(&t)
-            var clientTime = (Double(mach_absolute_time() * UInt64(t.numer)) / Double(UInt64(t.denom)) / 1000000)
-            var time: CLong = CLong(clientTime - self.offset)
-            var futureTime: CLong = (time + 5000) / 10000 * 10000
-            if futureTime - time < 5000 {
-                futureTime += 10000
-            }
-            clientTime = Double(mach_absolute_time() * UInt64(t.numer)) / Double(UInt64(t.denom)) / 1000000
-            self.delay = Double(futureTime + CLong(self.offset) - CLong(clientTime)) / 1000.0
-            println("offset is \(self.offset)")
-            weak var weakSelf = self
+            processOffsets(CLong(self.offset))
         } else if name == "play" {
             self.playButtonLabel.text = "Stop"
             
@@ -158,6 +142,51 @@ class ViewController: UIViewController, SocketIODelegate {
                 Int64(delay * Double(NSEC_PER_SEC))
             ),
             dispatch_get_main_queue(), closure)
+    }
+    
+    func getOffset(offsets: [CLong]) -> CLong {
+        var counters = [CInt]()
+        var results = [CInt]()
+        for var i = 0; i < offsets.count; ++i {
+            var counter: CInt = 0
+            var sum: CLong = 0
+            for otherOffset in offsets {
+                if abs(offsets[i] - otherOffset) <= offsetTolerance {
+                    counter++
+                    sum += CLong(counter)
+                }
+            }
+            counters[i] = counter
+            if counter != 0 {
+                results[i] = CInt(Double(sum) / Double(counter))
+            }
+        }
+        var max: CInt = 0
+        var maxIndex = 0
+        for var i = 0; i < counters.count; ++i {
+            max = counters[i]
+            maxIndex = i
+        }
+        if max == 0 {
+            return CLong(0)
+        } else {
+            return CLong(results[maxIndex])
+        }
+    }
+    
+    func processOffsets(offset: CLong) {
+        if self.offsetCounter < numberOfOffsets {
+            self.offsets[self.offsetCounter] = offset
+            ++offsetCounter
+            if offsetCounter < numberOfOffsets {
+                sleep(self.syncDelay)
+                self.socket.sendEvent("client_sync", withData: nil)
+            }
+        }
+        if self.offsetCounter >= self.numberOfOffsets {
+            self.offset = Double(getOffset(offsets))
+            self.synchronized = true
+        }
     }
 }
 
